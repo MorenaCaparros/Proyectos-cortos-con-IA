040bb860704b84daae6ea3816f013fbb5d4e057b
MorenaCaparros <morens217@gmail.com>
2023-03-06 16:43:21 -0300

    Add new function about what the AI receives

--- README.md
+++
@@ -1,1 +0,0 @@
-# Proyectos-cortos-con-IA
---
+++ chatapp-with-voice-and-openai-outline/worker.py
@@ -0,0 +1,44 @@
+import openai
+import requests
+
+openai.api_key = "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"
+
+
+def speech_to_text(audio_binary):
+    # Set up Watson Speech to Text HTTP Api url
+    base_url = 'https://sn-watson-stt.labs.skills.network'
+    api_url = base_url+'/speech-to-text/api/v1/recognize'
+    
+    # Set up parameters for our HTTP request
+    params = {
+        'model': 'en-US_Multimedia',
+    }
+
+    # Set up the body of our HTTP request
+    body = audio_binary
+
+    # Send a HTTP Post request
+    response = requests.post(api_url, params=params, data=audio_binary).json()
+
+    # Parse the response to get our transcribed text
+    text = 'null'
+    while bool(response.get('results')):
+        print('speech to text response:', response)
+        text = response.get('results').pop().get('alternatives').pop().get('transcript')
+        print('recognised text: ', text)
+        return text
+
+
+def text_to_speech(text, voice=""):
+    return None
+
+
+def openai_process_message(user_message):
+    # Set the prompt for OpenAI Api
+    prompt = "\"Act like a personal assistant. You can respond to questions, translate sentences, summarize news, and give recommendations. " + user_message + "\""
+    # Call the OpenAI Api to process our prompt
+    openai_response = openai.Completion.create(model="text-davinci-003", prompt=prompt,max_tokens=4000)
+    print("openai response:", openai_response)
+    # Parse the response to get the response text for our prompt
+    response_text = openai_response.choices[0].text
+    return response_text
